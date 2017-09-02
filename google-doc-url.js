10e041de2246956f8749a81aa90bda2a95473598
Reuben Kaponde <21077222+iammash@users.noreply.github.com>
2017-09-02 16:59:34 +0200

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1ZxvU8eGyuN9M8GxTU9acKVJv70iC3px_m3EVFsOHN9g/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1pnHmvQdt44e2Ql3jBq0N_M5DL9p5gRKMcqs49ALdffA/edit#gid=0';
